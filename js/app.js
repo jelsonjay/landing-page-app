322270d23ae3373ad961bd54b685b41e169ae2db
Jelson J <jelson20@outlook.com>
2021-08-30 22:50:00 +0100

    update changes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 const btnMobile = document.getElementById("btn-mobile")
 
-function toggleMenu(){
+function toggleMenu(e){
+if(e.type === "touchstart") e.preventDefault();
+
 const nav = document.getElementById("nav")
 nav.classList.toggle("active")
+const active = nav.classList.contains("active")
+e.currentTarget.setAttribute("aria-expanded" , active)
+if(e) {e.currentTarget.setAttribute("aria-label", "Close Menu")
+}else {
+  e.currentTarget.setAttribute("aria-label", "Open Menu")
+}
 }
 
 btnMobile.addEventListener("click" , toggleMenu)
-
+btnMobile.addEventListener("touchstart" , toggleMenu)
 
 
