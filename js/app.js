8516fe1232e2b5be9f582fa0109995acf6f384fe
Jelson J <jelson20@outlook.com>
2021-08-30 22:10:31 +0100

    update javascript app file

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const btnMobile = document.getElementById("btn-mobile")
+
+function toggleMenu(){
+const nav = document.getElementById("nav")
+nav.classList.toggle("active")
+}
+
+btnMobile.addEventListener("click" , toggleMenu)
+
+
+
